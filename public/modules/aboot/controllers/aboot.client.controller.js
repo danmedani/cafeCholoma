6531be0ddac71c782246774c2899ea0cb07847eb
danmedani <danmedani@gmail.com>
2015-06-30 19:32:22 -0700

    aboot x-cros!

--- public/modules/aboot/controllers/aboot.client.controller.js
+++ public/modules/aboot/controllers/aboot.client.controller.js
@@ -1,38 +1,32 @@
 'use strict';
 
-
 angular.module('aboot').controller('AbootController', ['$scope', 'Authentication', 'Aboots',
 	function($scope, Authentication, Aboots) {
-		// This provides Authentication context.
 		$scope.authentication = Authentication;
 
-		$scope.abootStuff = 'Cafe Honduras is all about selling coffee to you and making money to give to the Honduran people to use for medicalizing their wounds.Cafe Honduras is all about selling coffee to you and making money to give to the Honduran people to use for medicalizing their wounds.';
-		// $scope.abootStuff = Aboots.list({});
+		// $scope.abootStuff = 'Cafe Honduras is all about selling coffee to you and making money to give to the Honduran people to use for medicalizing their wounds.Cafe Honduras is all about selling coffee to you and making money to give to the Honduran people to use for medicalizing their wounds.';
+		
+		$scope.getAboot = function() {
+			Aboots.query(function(t) {
+				$scope.abootStuff = t[0].content;
+			});
+		};
 
-		// $scope.find = function() {
-		// 	// $scope.articles = Aboot.list();
-		// };
+		$scope.create = function() {
+			var newAboot = new Aboots({
+				title: this.title,
+				content: this.content
+			});
 
-		// $scope.create = function() {
-		// 	var aboot = new Aboots({
-		// 		title: this.title,
-		// 		content: this.content
-		// 	});
-		// };
+			newAboot.$save(function(response) {
+				//$location.path('articles/' + response._id);
 
-		// 	aboot.parent = 'top'; //by default the articles list only shows where parent = 'top'
-		// 	aboot.user = this.user;
-
-		// 	aboot.$save(function(response) {
-		// 		//$location.path('articles/' + response._id);
-
-		// 		$scope.title = '';
-		// 		$scope.content = '';
-		// 		// $scope.articles.unshift(article); //push it to the display
-		// 		$scope.createVisible = !$scope.createVisible;
-		// 	}, function(errorResponse) {
-		// 		$scope.error = errorResponse.data.message;
-		// 	});
-		// };
+				$scope.title = '';
+				$scope.content = '';
+				$scope.getAboot();
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
 	}
 ]);
--- public/modules/aboot/services/aboot.client.service.js
+++ public/modules/aboot/services/aboot.client.service.js
@@ -1,10 +1,6 @@
 'use strict';
 angular.module('aboot').factory('Aboots', ['$resource',
 	function($resource) {
-		return $resource('aboot', {}, {
-			list: {
-				method: 'GET'
-			}
-		});
+		return $resource('aboot', {});
 	}
 ]);
